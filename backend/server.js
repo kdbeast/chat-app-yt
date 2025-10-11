63fb7c2744bdc5bf6d226a686cb7094056c99d4e
Karan <karanjamwal2004@gmail.com>
2025-10-11 20:07:14 +0530

    user fetching api created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:5000/
