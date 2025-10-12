d5df3d4b75cd6aac27f11f0c7c9f8ebc3dd76ac1
Karan <karanjamwal2004@gmail.com>
2025-10-12 12:24:10 +0530

    sidebar created

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import MessageContainer from "../../components/MessageContainer";
+import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
