f9f306b2d083f56ffb9a11e3f2d072bd4a351d5f
Karan <karanjamwal2004@gmail.com>
2025-10-12 11:31:33 +0530

    home, login and signup created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Home />
+    </div>
   );
 }
 
