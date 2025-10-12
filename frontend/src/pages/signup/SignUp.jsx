a0d09ad4293032412d97b83432bf50f3257252ed
Karan <karanjamwal2004@gmail.com>
2025-10-12 19:04:01 +0530

    signup finished

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -108,8 +108,15 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+            <button
+              disabled={loading}
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
