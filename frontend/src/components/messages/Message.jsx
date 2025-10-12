f90a64dd8da114008805dc200cb019c814bccdbd
Karan <karanjamwal2004@gmail.com>
2025-10-12 12:24:29 +0530

    message container created

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,56 @@
+// import { useAuthContext } from "../../context/AuthContext";
+// import { extractTime } from "../../utils/extractTime";
+// import useConversation from "../../zustand/useConversation";
+
+const Message = () => {
+  //   const { authUser } = useAuthContext();
+  //   const { selectedConversation } = useConversation();
+  //   const fromMe = message.senderId === authUser._id;
+  //   const formattedTime = extractTime(message.createdAt);
+  // const chatClassName = fromMe ? "chat-end" : "chat-start";
+  //   const profilePic = fromMe
+  //     ? authUser.profilePic
+  //     : selectedConversation?.profilePic;
+  //   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+  //   const shakeClass = message.shouldShake ? "shake" : "";
+
+  return (
+    <>
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+            />
+          </div>
+        </div>
+        <div className="chat-header">
+          Obi-Wan Kenobi
+          <time className="text-xs opacity-50">12:45</time>
+        </div>
+        <div className="chat-bubble">You were the Chosen One!</div>
+        <div className="chat-footer opacity-50">Delivered</div>
+      </div>
+
+      <div className="chat chat-end">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
+            />
+          </div>
+        </div>
+        <div className="chat-header">
+          Anakin
+          <time className="text-xs opacity-50">12:46</time>
+        </div>
+        <div className="chat-bubble">I hate you!</div>
+        <div className="chat-footer opacity-50">Seen at 12:46</div>
+      </div>
+    </>
+  );
+};
+export default Message;
